def19b38c0089dfc1e9f779cd6379a1f21eb6b6b
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2017-06-13 19:27:56 -0400

    Added Projects

---
+++ mutli-module-application/dao/src/main/java/com/ihg/internaltools/pfm/DaoApplication.java
@@ -0,0 +1,12 @@
+package com.ihg.internaltools.pfm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoApplication.class, args);
+	}
+}
