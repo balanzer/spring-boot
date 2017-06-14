4c34301d09a158ab7d82801fd2db9284f03fc56d
Muralitharan Varatharajan <muralitharan.varatharajan@ihg.com>
2017-06-14 08:57:26 -0400

    Added API project

--- mutli-module-application/api/src/main/java/com/ihg/internaltools/pfm/api/WelcomeService.java
+++ mutli-module-application/api/src/main/java/com/ihg/internaltools/pfm/api/WelcomeService.java
@@ -5,6 +5,8 @@
  */
 package com.ihg.internaltools.pfm.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WelcomeService.class);
+
     @Value("${default.message:Default message not found.}")
     private String defaultMessage;
 
     @RequestMapping("/")
     public String greetings() {
+        LOGGER.debug("Welcome - debug {} ", this.defaultMessage);
+        LOGGER.error("Welcome - error {} ", this.defaultMessage);
+        LOGGER.info("Welcome - info {} ", this.defaultMessage);
         return this.defaultMessage;
     }
 }
